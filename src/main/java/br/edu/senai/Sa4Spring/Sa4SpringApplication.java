42e0ab9ba74edb2afbd49916cb863377cc06c795
Leonardo Felipe R. <leonardo.p.froncolato@gmail.com>
2023-10-30 15:03:18 -0300

    Git para GitHub V0.1

---
+++ src/main/java/br/edu/senai/Sa4Spring/Sa4SpringApplication.java
@@ -0,0 +1,13 @@
+package br.edu.senai.Sa4Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa4SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sa4SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/senai/Sa4Spring/Sa4SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.senai.Sa4Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa4SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
